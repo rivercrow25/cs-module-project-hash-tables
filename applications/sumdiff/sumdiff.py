576cec60c171b0ab1d3a39905ddde07c0b2a9d42
nick migel <nickmigel2@gmail.com>
2020-08-06 17:21:18 -0400

    welp its not optimized but hey dont judge

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,13 +3,23 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
+q = set(range(1, 10))
 #q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
+# q = (1, 3, 4, 7, 12)
 
 
 def f(x):
     return x * 4 + 6
 
-# Your code here
 
+# Your code here
+counter = 0
+
+for i in q:
+    for j in q:
+        for k in q:
+            for l in q:
+                if (f(i) + f(j)) == (f(k) - f(l)):
+                    counter += 1
+
+print(counter)
